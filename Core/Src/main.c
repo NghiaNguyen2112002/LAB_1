dbf4b68a1a1d6b658ab86ed45ec9d3c216ce7320
NghiaNguyen2112002 <113238175+NghiaNguyen2112002@users.noreply.github.com>
2022-09-24 07:38:08 +0700

    ex5

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -41,6 +41,17 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+#define RED		0
+#define YELLOW 	1
+#define GREEN	2
+static uint32_t led7Seg1[10] = { 0x003F0040, 0x00060079, 0x005B0024,
+  								0x004F0030, 0x00660019, 0x006D0012,
+  								0x007D0002, 0x00070078, 0x007F0000,
+  								0x006F0010 };
+static uint32_t led7Seg2[10] = { 0x3F004000, 0x06007900, 0x5B002400,
+								0x4F003000, 0x66001900, 0x6D001200,
+								0x7D000200, 0x07007800, 0x7F000000,
+								0x6F001000 };
 
 /* USER CODE END PV */
 
@@ -48,6 +59,9 @@
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
+
+void DisplayLight(uint8_t light1, uint8_t light2);
+void Display7Seg(uint8_t time1, uint8_t time2);
 
 /* USER CODE END PFP */
 
@@ -85,11 +99,19 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  uint16_t counter = 0;
-  uint16_t RED_TIME_COUNTER = 5;
-  uint16_t YELLOW_TIME_COUNTER = 2;
-  uint16_t GREEN_TIME_COUNTER = 3;
-  uint8_t  flagCode = 0;   //red : 0,  yellow : 1, green : 2
+
+  uint8_t time1 = 0, time2 = 0;
+  uint8_t light1 = 0, light2 = 0;
+
+  uint8_t RED_COUNTER = 5;
+  uint8_t GREEN_COUNTER = 3;
+  uint8_t YELLOW_COUNTER = 2;
+
+
+  light1 = RED;
+  time1 = RED_COUNTER;
+  light2 = GREEN;
+  time2 = GREEN_COUNTER;
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -97,32 +119,40 @@ int main(void)
   while (1)
   {
 
-	  if(counter <= 0){
-		  if(flagCode == 0){
-			  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin, GPIO_PIN_RESET);
-			  HAL_GPIO_WritePin(GPIOA, LED_YELLOW_Pin, GPIO_PIN_SET);
-			  HAL_GPIO_WritePin(GPIOA, LED_GREEN_Pin, GPIO_PIN_SET);
-			  flagCode = 1;
-			  flagCode = 1;
-			  counter = RED_TIME_COUNTER;
+	  if(time1 <= 0){
+		  if(light1 == RED){
+			  light1 = GREEN;
+			  time1 = GREEN_COUNTER;
 		  }
-		  else if(flagCode == 1){
-			  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin, GPIO_PIN_SET);
-			  HAL_GPIO_WritePin(GPIOA, LED_YELLOW_Pin, GPIO_PIN_RESET);
-			  HAL_GPIO_WritePin(GPIOA, LED_GREEN_Pin, GPIO_PIN_SET);
-			  flagCode = 2;
-			  counter = YELLOW_TIME_COUNTER;
+		  else if(light1 == YELLOW){
+			  light1 = RED;
+			  time1 = RED_COUNTER;
 		  }
-		  else if(flagCode == 2){
-			  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin, GPIO_PIN_SET);
-			  HAL_GPIO_WritePin(GPIOA, LED_YELLOW_Pin, GPIO_PIN_SET);
-			  HAL_GPIO_WritePin(GPIOA, LED_GREEN_Pin, GPIO_PIN_RESET);
-			  flagCode = 0;
-			  counter = GREEN_TIME_COUNTER;
+		  else if(light1 == GREEN){
+			  light1 = YELLOW;
+			  time1 = YELLOW_COUNTER;
 		  }
 	  }
+	  if(time2 <= 0){
+		  if(light2 == RED){
+			  light2 = GREEN;
+			  time2 = GREEN_COUNTER;
+		  }
+		  else if(light2 == YELLOW){
+			  light2 = RED;
+			  time2 = RED_COUNTER;
+		  }
+		  else if(light2 == GREEN){
+			  light2 = YELLOW;
+			  time2 = YELLOW_COUNTER;
+		  }
+	  }
+
+	  time1--, time2--;
+	  DisplayLight(light1, light2);
+	  Display7Seg(time1, time2);
 	  HAL_Delay(1000);
-	  counter--;
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -177,21 +207,55 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, red_1_Pin|yellow_1_Pin|green_1_Pin|red_2_Pin
+                          |yellow_2_Pin|green_2_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin LED_GREEN_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin;
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_10
+                          |GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14
+                          |GPIO_PIN_15|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5
+                          |GPIO_PIN_6|GPIO_PIN_8, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : red_1_Pin yellow_1_Pin green_1_Pin red_2_Pin
+                           yellow_2_Pin green_2_Pin */
+  GPIO_InitStruct.Pin = red_1_Pin|yellow_1_Pin|green_1_Pin|red_2_Pin
+                          |yellow_2_Pin|green_2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
+  /*Configure GPIO pins : PB0 PB1 PB2 PB10
+                           PB11 PB12 PB13 PB14
+                           PB15 PB3 PB4 PB5
+                           PB6 PB8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_10
+                          |GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14
+                          |GPIO_PIN_15|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5
+                          |GPIO_PIN_6|GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
 
+void DisplayLight(uint8_t light1, uint8_t light2){
+	uint32_t light1_BSRR[3] = { 0x00C00020, 0x00A00040, 0x00600080 };
+	uint32_t light2_BSRR[3] = { 0x06000100, 0x05000200, 0x00300400 };
+	GPIOA->BSRR = light1_BSRR[light1] + light2_BSRR[light2];
+}
+
+void Display7Seg(uint8_t time1, uint8_t time2){
+	if((time1 >= 0 && time1 <= 9) && (time2 >= 0 && time2 <= 9)){
+		GPIOB->BSRR = led7Seg1[time1] | led7Seg2[time2];
+	}
+}
 /* USER CODE END 4 */
 
 /**
